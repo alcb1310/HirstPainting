4369e06753ffd478bea7e0145cd3c31943a63134
Andrés Court <andres@andrescourt.com>
2022-09-11 17:54:50 -0500

    Hide the turtle, so it does not show when drawing the dots, it improves speed

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+import turtle
 from random import choice
 from data import color_list
 import turtle as t
 
 t.colormode(255)
-timmy = t.Turtle()
-timmy.speed(0)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+for _ in range(10):
+    tim.fd(30)
+tim.setheading(0)
 
 for _ in range(10):
     for _ in range(10):
         dot_color = choice(color_list)
-        timmy.penup()
-        timmy.dot(20, dot_color)
-        timmy.fd(30)
+        tim.dot(20, dot_color)
+        tim.fd(30)
 
-    timmy.lt(90)
-    timmy.fd(30)
-    timmy.lt(90)
-    timmy.fd(300)
-    timmy.rt(180)
-
+    tim.lt(90)
+    tim.fd(30)
+    tim.lt(90)
+    tim.fd(300)
+    tim.rt(180)
 
 screen = t.Screen()
 screen.exitonclick()
